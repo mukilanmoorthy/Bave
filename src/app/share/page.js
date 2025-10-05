06f8e273b2906f7e8c6e7e7bbee66c02c56c0f18
Mukilan1806 <mukilan.m@growthfusion.co>
2025-10-05 18:45:07 +0530

    add

--- src/app/share/page.js
+++ src/app/share/page.js
@@ -3,29 +3,28 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function SharPage(){
-     const searchParms = useSearchParams();
-     const [sharedLink , setSharedLink] = useState(null);
+export default function SharePage() {
+  const searchParams = useSearchParams();
+  const [sharedLink, setSharedLink] = useState("");
 
+  useEffect(() => {
+    const url = searchParams.get("url");
+    if (url) setSharedLink(url);
+  }, [searchParams]);
 
-     useEffect(()=>{
-        const url = searchParms.get("url");
-        if(url){
-            setSharedLink(url);
-        }
-     },[searchParms]);
-     
-    return(
-        <div>
-            <h2>Shared to Bave🫦</h2>
-            {sharedLink ? (
-                <>
-                <p>Recived Link:</p>
-                <a href={sharedLink} target="_blank">{sharedLink}</a>
-                </>
-            ) :(
-                <p>No link recived</p>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <h2>Shared to Bave🫦</h2>
+      {sharedLink ? (
+        <>
+          <p>Received Link:</p>
+          <a href={sharedLink} target="_blank" rel="noopener noreferrer">
+            {sharedLink}
+          </a>
+        </>
+      ) : (
+        <p>No link received</p>
+      )}
+    </div>
+  );
 }
