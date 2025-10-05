6bb3b96be60bf2a5239adb3f592b5891563f8372
Mukilan1806 <mukilan.m@growthfusion.co>
2025-10-05 18:55:02 +0530

    add

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true, // disable PWA to test build
 });
 
 module.exports = withPWA({
