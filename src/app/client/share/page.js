6a04edec1246ade47172370c1b7789c3019406d0
Mukilan1806 <mukilan.m@growthfusion.co>
2025-10-12 19:06:51 +0530

    add

--- src/app/client/share/page.js
+++ src/app/client/share/page.js
@@ -1,17 +1,14 @@
-"use client";
-
 import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function SharePage() {
-  const searchParams = useSearchParams();
+  const router = useRouter();
   const [shareData, setShareData] = useState(null);
 
   useEffect(() => {
-    const title = searchParams.get("title");
-    const text = searchParams.get("text");
-    const url = searchParams.get("url");
+    const query = router.query;
+    const { title, text, url } = query;
 
     if (title || text || url) {
       const newShare = { title, text, url, time: new Date().toString() };
@@ -20,7 +17,7 @@ export default function SharePage() {
       localStorage.setItem("bave_shares", JSON.stringify(storedShares));
       setShareData(newShare);
     }
-  }, [searchParams]);
+  }, [router.query]);
 
   if (!shareData) return <p>Processing shared content...</p>;
 
